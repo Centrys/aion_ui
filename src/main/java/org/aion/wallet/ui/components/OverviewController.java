8a8968180477364ac10c42e52010593ca906662a
Cristian Ilca <cristian.ilca@centrys.io>
2018-09-28 15:36:17 +0300

    Managed to get a transaction hash

--- src/main/java/org/aion/wallet/ui/components/OverviewController.java
+++ src/main/java/org/aion/wallet/ui/components/OverviewController.java
@@ -20,12 +20,22 @@ import org.aion.wallet.log.WalletLoggerFactory;
 import org.aion.wallet.ui.components.partials.AddAccountDialog;
 import org.aion.wallet.ui.components.partials.ImportAccountDialog;
 import org.aion.wallet.ui.components.partials.UnlockMasterAccountDialog;
+import org.bouncycastle.util.encoders.Hex;
 import org.slf4j.Logger;
+import org.web3j.crypto.*;
+import org.web3j.protocol.Web3j;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
+import org.web3j.protocol.core.methods.response.EthSendTransaction;
+import org.web3j.protocol.http.HttpService;
 
+import java.io.IOException;
+import java.math.BigInteger;
 import java.net.URL;
 import java.util.EnumSet;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutionException;
 
 public class OverviewController extends AbstractController {
 
@@ -141,5 +151,51 @@ public class OverviewController extends AbstractController {
             return;
         }
         addAccountDialog.open(mouseEvent);
+    }
+
+    public void attemptHardcodedSwap(final MouseEvent mouseEvent) {
+        Web3j web3j = Web3j.build(new HttpService("https://rinkeby.infura.io/v3/00912daf483e4a7fb8d2e8f320d3393c"));  // defaults to http://localhost:8545/
+        Credentials credentials = null;
+        try {
+            credentials = WalletUtils.loadCredentials("testtesttest", "/home/cristi-i-workstation/Downloads/UTC--2018-09-18T06-59-01.849Z--bed34249b87049526a90592ad4719dc1bf92a0f7");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (CipherException e) {
+            e.printStackTrace();
+        }
+
+// get the next available nonce
+        EthGetTransactionCount ethGetTransactionCount = null;
+        try {
+            ethGetTransactionCount = web3j.ethGetTransactionCount(
+                    "0xbEd34249b87049526A90592AD4719Dc1Bf92A0F7", DefaultBlockParameterName.LATEST).sendAsync().get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
+
+// create our transaction
+        RawTransaction rawTransaction  = RawTransaction.createEtherTransaction(
+                nonce,
+                BigInteger.valueOf(53000),
+                BigInteger.valueOf(100000),
+                "0x37Fe03E7ffeA820fEd48f352047Cb7F63fddE554",
+                BigInteger.valueOf(1));
+
+// sign & send our transaction
+        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
+        String hexValue = "0x" + Hex.toHexString(signedMessage);
+        try {
+            EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).send();
+            System.out.println(ethSendTransaction.getTransactionHash());
+            if(ethSendTransaction.hasError()) {
+                System.out.println(ethSendTransaction.getError().getMessage());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+// ...
     }
 }
--- src/main/java/org/aion/wallet/ui/components/partials/AboutPage.java
+++ src/main/java/org/aion/wallet/ui/components/partials/AboutPage.java
@@ -1,62 +1,11 @@
 package org.aion.wallet.ui.components.partials;
 
 import javafx.scene.input.MouseEvent;
-import org.bouncycastle.util.encoders.Hex;
-import org.web3j.crypto.*;
-import org.web3j.protocol.Web3j;
-import org.web3j.protocol.core.DefaultBlockParameterName;
-import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
-import org.web3j.protocol.core.methods.response.EthSendTransaction;
-import org.web3j.protocol.http.HttpService;
-
-import java.io.IOException;
-import java.math.BigInteger;
-import java.util.concurrent.ExecutionException;
 
 public class AboutPage {
     private final AboutDialog aboutDialog = new AboutDialog();
 
     public void openAboutDialog(MouseEvent mouseEvent) {
         aboutDialog.open(mouseEvent);
-        Web3j web3j = Web3j.build(new HttpService("https://rinkeby.infura.io/v3/00912daf483e4a7fb8d2e8f320d3393c"));  // defaults to http://localhost:8545/
-        Credentials credentials = null;
-        try {
-            credentials = WalletUtils.loadCredentials("testtesttest", "/home/cristi-i-workstation/Downloads/UTC--2018-09-18T06-59-01.849Z--bed34249b87049526a90592ad4719dc1bf92a0f7");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (CipherException e) {
-            e.printStackTrace();
-        }
-
-// get the next available nonce
-        EthGetTransactionCount ethGetTransactionCount = null;
-        try {
-            ethGetTransactionCount = web3j.ethGetTransactionCount(
-                    "0xbEd34249b87049526A90592AD4719Dc1Bf92A0F7", DefaultBlockParameterName.LATEST).sendAsync().get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
-        BigInteger nonce = ethGetTransactionCount.getTransactionCount();
-
-// create our transaction
-        RawTransaction rawTransaction  = RawTransaction.createEtherTransaction(
-                nonce,
-                BigInteger.valueOf(22000),
-                BigInteger.valueOf(41),
-                "0x37Fe03E7ffeA820fEd48f352047Cb7F63fddE554",
-                BigInteger.valueOf(1));
-
-// sign & send our transaction
-        byte[] signedMessage = TransactionEncoder.signMessage(rawTransaction, credentials);
-        String hexValue = Hex.toHexString(signedMessage);
-        try {
-            EthSendTransaction ethSendTransaction = web3j.ethSendRawTransaction(hexValue).send();
-            System.out.println(ethSendTransaction.getTransactionHash());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-// ...
     }
 }
