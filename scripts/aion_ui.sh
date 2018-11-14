0fd4052ca2c7e80b3d3f7edaa28419f31e0b9db5
Cristian Ilca <cristian.ilca@centrys.io>
2018-11-14 19:32:17 +0200

    Added date to log files

--- scripts/aion_ui.sh
+++ scripts/aion_ui.sh
@@ -18,6 +18,7 @@ STORAGE_DIR=${HOME}/.aion
 LOG_DIR=${STORAGE_DIR}/log
 JAVA_INSTALL=${STORAGE_DIR}/java
 JAVA_CMD=${JAVA_INSTALL}/bin/java
+CURRENT_DATE=`date '+%Y-%m-%d_%H:%M:%S'`
 
 if [[ ! -f ${JAVA_CMD} ]] || [[ $(${JAVA_CMD} -version 2>&1 | grep "${JAVA_VERSION}" | wc -l) -lt 1 ]]; then
   unzip java.zip -d ${STORAGE_DIR}
@@ -28,4 +29,4 @@ LIB_DIR=${SCRIPT_PATH}/lib/*
 
 mkdir -p ${LOG_DIR}
 
-${JAVA_CMD} -cp "${MOD_DIR}:${LIB_DIR}" -Dlocal.storage.dir=${STORAGE_DIR} -Xms300m -Xmx500m org.aion.wallet.WalletApplication &> ${LOG_DIR}/log &
+${JAVA_CMD} -cp "${MOD_DIR}:${LIB_DIR}" -Dlocal.storage.dir=${STORAGE_DIR} -Xms300m -Xmx500m org.aion.wallet.WalletApplication &> ${LOG_DIR}/log_${CURRENT_DATE} &
